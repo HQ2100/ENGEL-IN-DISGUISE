7f6bf012e1fd7271b6a0b7b1c91198074ca4f007
HQ <HQ@LAPTOP-9CUI9RBK>
2024-06-02 13:45:56 +0800

    Log In function

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#1e212b',
+    primary: '#4bb0eb',
+    secondary: '#1666a8',
+    error: '#ED1C24',
+  },
+}
